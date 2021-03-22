9104ec9acfa50328655318569282c9819dc559d9
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-22 13:50:40 -0700

    getName second test pass

--- get-name.test.js
+++ get-name.test.js
@@ -11,10 +11,22 @@ describe('get name from object function', () => {
             name: 'spot',
             age: 5,
             weight: '20 lbs'
-        }
+        };
 
         const name = getName(spot);
 
         expect(name).toEqual('spot');
+    });
+
+    it('takes in an object and returns a name value', () => {
+
+        const character = {
+            _id: '5cf5679a915ecad153ab68c9',
+            name: 'Aang'
+        };
+
+        const name = getName(character);
+
+        expect(name).toEqual('Aang');
     });
 });
