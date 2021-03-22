462882e5f5e299f5703dff4878069d09f1241965
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-22 15:54:42 -0700

    capitalizeAndFilter refactor test pass

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,11 +1,17 @@
 const capitalizeAndFilter = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        array[i] = array[i].toUpperCase();
-        if (array[i][0] === 'F') {
-            array.splice([i]);
-        }
-    }
-    return array;
+    const upperCase = array.map(word => word.toUpperCase()).filter(word => word.charAt(0) !== 'F');
+    return upperCase;
+
 };
+
+// const capitalizeAndFilter = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         array[i] = array[i].toUpperCase();
+//         if (array[i][0] === 'F') {
+//             array.splice([i]);
+//         }
+//     }
+//     return array;
+// };
 
 module.exports = capitalizeAndFilter;
